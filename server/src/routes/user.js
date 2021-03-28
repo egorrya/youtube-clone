e03030148d8262f5450d0f86cc21dbb0b05a1eef
Egor Rya <fayncore@gmail.com>
2021-03-28 03:48:15 +0300

    api get recommended channels

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -9,6 +9,7 @@ function getUserRoutes() {
   const router = express.Router();
 
   // ./api/v1/users
+  router.get("/", protect, getRecommendedChannels);
   router.get("/liked-videos", protect, getLikedVideos);
   router.get("/history", protect, getHistory);
   router.get("/:userId/toggle-subscribe", protect, toggleSubscribe);
@@ -215,7 +216,55 @@ async function searchUser(req, res, next) {
   res.status(200).json({ users });
 }
 
-async function getRecommendedChannels(req, res) {}
+async function getRecommendedChannels(req, res) {
+  const channels = await prisma.user.findMany({
+    where: {
+      id: {
+        not: req.user.id,
+      },
+    },
+    take: 10,
+  });
+
+  if (!channels.length) {
+    return res.status(200).json({ channels });
+  }
+
+  for (const channel of channels) {
+    const subscribersCount = await prisma.subscription.count({
+      where: {
+        subscribedToId: {
+          equals: channel.id,
+        },
+      },
+    });
+
+    const videosCount = await prisma.video.count({
+      where: {
+        userId: channel.id,
+      },
+    });
+
+    const isSubscribed = await prisma.subscription.findFirst({
+      where: {
+        AND: {
+          subscriberId: {
+            equals: req.user.id,
+          },
+          subscribedToId: {
+            equals: channel.id,
+          },
+        },
+      },
+    });
+
+    channel.subscribersCount = subscribersCount;
+    channel.videosCount = videosCount;
+    channel.isSubscribed = Boolean(isSubscribed);
+  }
+
+  res.status(200).json({ channels });
+}
 
 async function getProfile(req, res, next) {}
 
